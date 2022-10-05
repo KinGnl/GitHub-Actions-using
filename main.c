1f27ad8538a20767b60945d7b1fc28c9b95c302d
Nick GNONLONFOUN <KiNGnL@localhost.localdomain>
2022-10-05 15:38:01 +0100

    feat(code) : Adding code files.

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** main.c
+** File description:
+** Just for the test of functions.
+*/
+
+#include "test.h"
+
+int main(void)
+{
+    my_putchar('N');
+    my_putchar('i');
+    my_putchar('c');
+    my_putchar('k');
+    my_putchar('\n');
+    return (0);
+}
---
+++ test.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** test.h
+** File description:
+** Contains function prototype.
+*/
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <unistd.h>
+
+void my_putchar(char);
+
+#endif
