4cb6d936f2b3e1a31047b1d3eb61e09197eeb399
Nick GNONLONFOUN <KiNGnL@localhost.localdomain>
2022-10-05 16:23:30 +0100

    feat(code): adding function to code

---
+++ test.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** test.c
+** File description:
+** Test.
+*/
+
+#include <stdio.h>
+
+void test(void)
+{
+    printf("%s\n", "It's just test my friend !");
+    return (0);
+}
