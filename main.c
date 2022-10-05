c770e88d421c028aac39f1e4a9a3046aecc2949c
Ashley Avocanh <ashley.avocanh@epitech.eu>
2022-10-05 15:46:10 +0100

    adding min function in main

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2022
+** main.c
+** File description:
+** Just for the test of functions.
+*/
+
+#include "test.h"
+
+int min(int a, int b)
+{
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
+int main(void)
+{
+    my_putchar('N');
+    my_putchar('i');
+    my_putchar('c');
+    my_putchar('k');
+    my_putchar('\n');
+    return (0);
+}
